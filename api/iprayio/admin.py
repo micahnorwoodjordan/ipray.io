30752bcefaab241fc44746dd47782712fdbf58d1
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-03 01:05:30 -0700

    integer-based model id + modeladmin

--- api/iprayio/admin.py
+++ api/iprayio/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Prayer
 
-# Register your models here.
+
+@admin.register(Prayer)
+class PrayerAdmin(admin.ModelAdmin):
+    # Columns to display in the list view
+    list_display = (
+        "id",
+        "created_at",
+        "fulfilled_at",
+        "source_email",
+        "source_ip_address",
+        "next_allowed_at",
+    )
+
+    # Filters on the right-hand sidebar
+    list_filter = (
+        "fulfilled_at",
+        "created_at",
+    )
+
+    # Searchable fields
+    search_fields = (
+        "text",
+        "source_email",
+        "source_ip_address",
+    )
+
+    # Fields that are read-only (can't be edited in admin)
+    readonly_fields = (
+        "id",
+        "created_at",
+        "source_ip_address",
+        "next_allowed_at",
+    )
+
+    # Default ordering
+    ordering = ("-created_at",)
--- api/iprayio/models.py
+++ api/iprayio/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Prayer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     fulfilled_at = models.DateTimeField(null=True, blank=True, help_text="When this prayer has been prayed over")
     text = models.TextField()
