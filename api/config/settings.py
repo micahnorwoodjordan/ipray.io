27cd7c865bfbfaaa174a31f12ffac4fe61c049dc
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-04 18:28:50 -0700

    updaet cors and csrf settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -28,10 +28,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+site_hosts = (
+    'https://ipray.io',
+    'https://api.ipray.io'
+)
+
+CSRF_TRUSTED_ORIGINS, CORS_ALLOWED_ORIGINS = site_hosts
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
