22011eb445e5c006373fca73d412e2d400922330
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-04 17:50:15 -0700

    allow all hosts

--- api/config/settings.py
+++ api/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!-i!)x65(-bk+t1jiet&b$t!c4rs)nk((9jckw&u@#-jh%)boj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
