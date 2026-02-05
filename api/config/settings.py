25b406ed1c74c02f91f21f5e5ee3746a71924c84
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-04 22:01:30 -0700

    use 0 or 1 to denote debug mode

--- api/config/settings.py
+++ api/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!-i!)x65(-bk+t1jiet&b$t!c4rs)nk((9jckw&u@#-jh%)boj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = bool(int(os.environ['DEBUG']))
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = '/static/'
