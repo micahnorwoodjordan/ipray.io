8575ffad0156dccb800bea98a10e904dbf7b4662
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-06 14:18:15 -0700

    bundle ui at runtime

--- ui/server.js
+++ ui/server.js
@@ -5,14 +5,11 @@ import url from "url";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const distPath = path.join(__dirname, "app", "dist");
-console.log(`NOODLES: ${distPath}`);
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   let filePath = req.url === "/" ? "/index.html" : req.url;
   filePath = path.join(distPath, filePath);
-
-  console.log(`URL: ${req.url} -- FP: ${filePath}`);
 
   if (req.url === '/favicon.ico') {
     filePath = path.join(distPath, 'favicon.ico');
@@ -20,7 +17,7 @@ const server = http.createServer((req, res) => {
 
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      console.log(err);
+      console.log(`could not find file: ${path.parse(filePath).base}`);
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Not found");
       return;
