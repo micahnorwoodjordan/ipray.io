e0acfcbb61abc06c6ca1f3e5e0760d06cfb29f85
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-16 22:07:15 -0700

    get prayer endpoint

--- api/iprayio/urls.py
+++ api/iprayio/urls.py
@@ -6,5 +6,6 @@ from iprayio import views
 urlpatterns = [
     path("ping", views.ping, name="ping"),
     path("prayers/create", views.create_prayer_request, name="prayer-create"),
-    path("prayers/<uuid:pk>/complete", views.complete_prayer_request, name="prayer-complete"),
+    path("prayer/<uuid:pk>/complete", views.complete_prayer_request, name="prayer-complete"),
+    path("prayer", views.get_prayer_request, name="prayer-get"),
 ]
