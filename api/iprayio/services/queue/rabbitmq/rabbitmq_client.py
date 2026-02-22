5e125dad1abe2e22b124316230782cf4e28b1a8c
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-22 08:20:29 -0700

    oops

--- api/iprayio/services/queue/rabbitmq/rabbitmq_client.py
+++ api/iprayio/services/queue/rabbitmq/rabbitmq_client.py
@@ -7,24 +7,26 @@ QUEUE_NAME = os.environ['QUEUE_NAME']
 QUEUE_EXCHANGE = os.environ['QUEUE_EXCHANGE']
 
 
+def callback(ch, method, properties, body):
+    print(body)
+
+
 def publish():
     params = pika.URLParameters(QUEUE_URL)
     params.socket_timeout = 5
     connection = pika.BlockingConnection(params)  # Connect to CloudAMQP
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE_NAME, durable=True)
-
     channel.basic_publish(exchange=QUEUE_EXCHANGE, routing_key='', body='test from micah macbook pro')
     connection.close()
-
     print("Message published. closing connection")
 
 
-def consume(msg):
+def consume():
     params = pika.URLParameters(QUEUE_URL)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE_NAME)
-    channel.basic_consume(QUEUE_NAME, print('sending email'), auto_ack=True)  # set up subscription on the queue
+    channel.basic_consume(QUEUE_NAME, callback, auto_ack=True)  # set up subscription on the queue
     channel.start_consuming()  # start consuming (blocks)
     connection.close()
