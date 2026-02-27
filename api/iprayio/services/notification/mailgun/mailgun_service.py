977ea26ef4adfbf224dba30f42b71f2d61408202
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-02-26 23:59:15 -0700

    rewrite remaining services and include proper logging

--- api/iprayio/services/notification/mailgun/mailgun_service.py
+++ api/iprayio/services/notification/mailgun/mailgun_service.py
@@ -9,33 +9,44 @@ from django.conf import settings
 from iprayio.models import Prayer
 
 
+class MailgunServiceException(Exception):
+    pass
+
+
 IS_PRODUCTION = not settings.DEBUG
-TEMPLATE_NAME = 'production' if IS_PRODUCTION else 'dev'
 
 
-def _send_email(to: list[str], subject: str, text: str, **kwargs) -> None:
-    data = {
-        **dict(kwargs),
-        **{
-            "from": settings.MAILGUN_FROM,
-            "to": to,
-            "subject": subject,
-            "text": text,
+class MailgunService:
+    def __init__(self):
+        self.sender = settings.MAILGUN_FROM
+        self._auth = ('api', settings.MAILGUN_API_KEY)
+        self.domain = settings.MAILGUN_DOMAIN
+        self.template_name = 'production' if IS_PRODUCTION else 'dev'
+        self.admin_to_email = settings.ADMIN_NOTIFICATION_EMAIL
+        self.url = 'https://api.mailgun.net/v3/' + self.domain + '/messages'
+
+    def _send_email(self, to: list[str], subject: str, text=None, **kwargs) -> None:
+        data = {
+            **dict(kwargs),
+            **{
+                "from": self.sender,
+                "to": to,
+                "subject": subject,
+                'text': text
+            }
         }
-    }
 
-    response = requests.post(f"https://api.mailgun.net/v3/{settings.MAILGUN_DOMAIN}/messages", auth=("api", settings.MAILGUN_API_KEY), data=data, timeout=10)
-    response.raise_for_status()
+        try:
+            response = requests.post(self.url, auth=self._auth, data=data, timeout=10)
+            response.raise_for_status()
+        except Exception as e:
+            raise MailgunServiceException from e
 
+    def send_admin_prayer_submission_notification(self, prayer: Prayer) -> None:
+        subject = f"ipray.io - Prayer Request: {prayer.id}"
+        text = prayer.text
+        self._send_email([self.admin_to_email], subject, text)
 
-def send_admin_prayer_submission_notification(prayer: Prayer) -> None:
-    subject = f"ipray.io - Prayer Request: {prayer.id}"
-    text = prayer.text
-    _send_email([settings.ADMIN_NOTIFICATION_EMAIL], subject, text)
-
-
-def send_user_prayer_completed_notification(prayer: Prayer) -> None:
-    user_name = prayer.user_name.capitalize() or "Friend"
-    subject = f'ipray.io - {user_name}, Your Prayer Request Has Been Lifted Up'
-    text = f'Hi {user_name},\nI just prayed over your prayer request:\n\n\n"{prayer.text}"'
-    _send_email([prayer.user_email], subject, text, template=TEMPLATE_NAME)
+    def send_user_prayer_completed_notification(self, prayer: Prayer) -> None:
+        subject = 'ipray.io - Hi friend, Your Prayer Request Has Been Lifted Up'
+        self._send_email([prayer.user_email], subject, template=self.template_name)
--- api/iprayio/services/notification/notification_service.py
+++ api/iprayio/services/notification/notification_service.py
@@ -1,12 +1,22 @@
 import socket
+import logging
 from enum import Enum
 from dataclasses import dataclass
 
 from django.conf import settings
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 from iprayio.models import Prayer
-from iprayio.services.notification.mailgun.mailgun_service import send_admin_prayer_submission_notification, send_user_prayer_completed_notification
+from iprayio.utilities import logging_utilities
+from iprayio.services.notification.mailgun.mailgun_service import MailgunService, MailgunServiceException
+
+
+logger = logging.getLogger(__name__)
+
+
+class NotificationServiceException(Exception):
+    pass
 
 
 WORKER_ID = socket.gethostname()
@@ -32,40 +42,49 @@ class NotificationService:
         self._mailgun_api_key = settings.MAILGUN_API_KEY
         self._mailgun_from = settings.MAILGUN_FROM
         self._admin_notification_email = settings.ADMIN_NOTIFICATION_EMAIL
+        self._service = MailgunService()
 
     def notify_admin(self, methods: list[NotificationMethod], prayer_id: int) -> NotificationSummary:
         prayer = Prayer.objects.get(id=prayer_id)
         summary = NotificationSummary(False, False, None, None, prayer)
 
-        if NotificationMethod.EMAIL.value in methods:
-            try:
-                send_admin_prayer_submission_notification(prayer)
+        try:
+            if NotificationMethod.EMAIL.value in methods:
+                self._service.send_admin_prayer_submission_notification(prayer)
                 summary.email_sent = True
-                print(f'sent email for prayer {prayer.id}')
-            except Exception as e:
-                print(f'failed to send email: {e}')
-                summary.email_error = str(e)
 
-        if NotificationMethod.SMS.value in methods:
-            try:
+            if NotificationMethod.SMS.value in methods:
                 # send_prayer_notification_sms(prayer)  # TODO: recent A2P regulations make simple SMS rigorous to get off the ground
                 summary.sms_sent = False  # TODO: flip once SMS is figured out
-                print(f'sent sms for prayer {prayer.id}')
-            except Exception as e:
-                print(f'failed to send sms: {e}')
-                summary.sms_error = str(e)
+
+        except ObjectDoesNotExist as e:
+            logging_utilities.log_typed_error(logger, e, f'could not notify admin of new prayer request because Prayer object with id {prayer_id} does not exist')
+
+        except MailgunServiceException as e:
+            logging_utilities.log_typed_error(logger, e, f'failed to notify admin via email of new prayer request: {prayer.id}')
+            summary.email_error = str(e)
+
+        except Exception as e:  # TODO: catch exectpion thrown in SMS service (after service gets built)
+            logging_utilities.log_typed_error(logger, e, f'an error occurred while notifying admin of new prayer request: {prayer.id}')
+            summary.sms_error = str(e)
 
         return summary
 
     def notify_user(self, prayer_id: int) -> None:
-        prayer = Prayer.objects.get(id=prayer_id)
+        try:
+            prayer = Prayer.objects.get(id=prayer_id)
 
-        if prayer.user_email is not None:
-            try:
-                send_user_prayer_completed_notification(prayer)
-                print(f'notified user by email for prayer {prayer.id}')
-            except Exception as e:
-                print(f'failed to notify user by email: {e}')
+            if prayer.user_email is not None:
+                self._service.send_user_prayer_completed_notification(prayer)
+
+        except ObjectDoesNotExist as e:
+            logging_utilities.log_typed_error(logger, e, f'could not notify user via email of prayer completion because Prayer object with id {prayer_id} does not exist')
+
+        except MailgunServiceException as e:
+            logging_utilities.log_typed_error(logger, e, f'failed to notify user via email of prayer completion {prayer.id}')
+
+        except Exception as e:
+            logger.error('an error occurred while attempting to notify user of prayer completion: %s', str(e))
 
     @staticmethod
     def update_prayer_status(summary):
